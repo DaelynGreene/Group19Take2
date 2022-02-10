fb1073a3a8151625a9e6b480beae84ad277c8e25
Zach Rosenberg <zrosenb1@vols.utk.edu>
2022-02-10 13:49:33 -0500

    Update ShinyApp.R

--- ShinyApp.R
+++ ShinyApp.R
@@ -1,10 +1,27 @@
 library(shiny)
+library(fpp3)
+library(plotly)
+data("tourism")
+MYDATA <- subset(tourism, tourism$Region == "Adelaide")
 
 ui <- fluidPage(
-  
+  sidebarLayout(
+    sidebarPanel(
+      
+    ),
+    
+    mainPanel(
+      plotOutput("plot"),
+      
+    )
+  )
 )
 
 server <- function(input, output, session) {
+  output$plot <- renderPlot({
+    autoplot(MYDATA)
+  })
+  
   
 }
 
